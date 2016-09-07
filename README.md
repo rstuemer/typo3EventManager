201ab454c2986ee277e359972218a3d928771682
robert.stuemer@googlemail.com <Schaschlik2go>
2016-09-07 10:50:47 +0200

    Day 2 disable Translation for felxForms , dont allow Events on standard pages

--- README.md
+++ README.md
@@ -26,6 +26,37 @@ Um zu verhindern das andere Extensions geladen werden minimale Extension mit ext
   	 Anlegen und verändern der Datenbank. 
   	 Immer Create Statements Typo3 parst diese Datei nochmal und ändert selbstständig auf ALTER wenn die Tablle  schon existiert.
   
+  ###ext_tables.php
+  	-Plugin Registrieren
+  	-Flexforms	einbinden
+  	-register Plugin in Template->Include List und lädt setup und constants Dateien -> 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript',
+        'typo3EventManager');
+     -addLLrefForTCAdescr -> ContextSensitive Hilfe Texte etc.
+     -allowTableOnStandardPages -> Kann der Datensatz auf Normalen Seiten angelegt werden. 
+     -makeCategorizable -> kategorisierbar machen   
+
+  	
+  	
+  	
+  ###ext_localconf.php 
+  	Hook Einbauen -> Beispiel ot_divider 
+  	
+  
+  
+##Interessante Links 
+	FileUpload -> https://github.com/plobacher/extbasebookexample , https://github.com/helhum/upload_example
+	Datenschutz-> Shariff
+	SRIHash.org -> CDN Dateien auf Veränderung checken	
+	Hooks: https://somethingphp.com/extending-classes-typo3/
+	Typo3 Context -> http://blog.marit.ag/2014/11/03/typo3-context-verstehen-und-anwenden/
+#Tips
+	-System Cache Leeren aktivieren ->Installtool-> Configuration Preset -> Debugsettings -> debug -> activate -> Custom -> remove deprecated log 
+##Performance 
+	-Redis-Server Cache 
+	-Varnish-Cache
+	-APC bzw APCU
+	-ViewHelper schneller wenn CompilableInterface Implementiert wird 
+
  
 
 
@@ -34,9 +65,17 @@ Um zu verhindern das andere Extensions geladen werden minimale Extension mit ext
 
 
 
+#TypoScript 
+	-Constanten Auslesen {$name}
 
 
 
 
+
+
+
+
+#phpStorm Key
+ strg + geteilt (auf nummernblock) -> Zeile aufkommentieren
 
 
--- typo3conf/ext/ot_events/ext_tables.php
+++ typo3conf/ext/ot_events/ext_tables.php
@@ -1,50 +1,61 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
+    die('Access denied.');
 }
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	'OliverThiele.' . $_EXTKEY,
-	'Pi1',
-	'Events'
+    'OliverThiele.' . $_EXTKEY,
+    'Pi1',
+    'Events'
 );
 
-$pluginSignature = str_replace('_','',$_EXTKEY) . '_pi1';
+$pluginSignature = str_replace('_', '', $_EXTKEY) . '_pi1';
+
+/**
+ * Add Flexform
+ */
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_pi1.xml');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature,
+    'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_pi1.xml');
 
 if (TYPO3_MODE === 'BE') {
 
-	/**
-	 * Registers a Backend Module
-	 */
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-		'OliverThiele.' . $_EXTKEY,
-		'web',	 // Make module a submodule of 'web'
-		'mod1',	// Submodule key
-		'',						// Position
-		array(
-			'Event' => 'list, show, new, create, edit, update, delete','EventLocation' => 'list, show, new, create, edit, update, delete','EventCategory' => 'list, show, new, create, edit, update, delete',
-		),
-		array(
-			'access' => 'user,group',
-			'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
-			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_mod1.xlf',
-		)
-	);
-
+    /**
+     * Registers a Backend Module
+     */
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+        'OliverThiele.' . $_EXTKEY,
+        'web',     // Make module a submodule of 'web'
+        'mod1',    // Submodule key
+        '',                        // Position z.b. after: {Extension}
+        array(
+            'Event' => 'list, show, new, create, edit, update, delete',
+            'EventLocation' => 'list, show, new, create, edit, update, delete',
+            'EventCategory' => 'list, show, new, create, edit, update, delete',
+        ),
+        array(
+            'access' => 'user,group',
+            'icon' => 'EXT:' . $_EXTKEY . '/ext_icon.png',
+            'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_mod1.xlf',
+        )
+    );
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'typo3EventManager');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_otevents_domain_model_event', 'EXT:ot_events/Resources/Private/Language/locallang_csh_tx_otevents_domain_model_event.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_otevents_domain_model_event');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript',
+    'typo3EventManager');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_otevents_domain_model_eventlocation', 'EXT:ot_events/Resources/Private/Language/locallang_csh_tx_otevents_domain_model_eventlocation.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_otevents_domain_model_eventlocation');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_otevents_domain_model_event',
+    'EXT:ot_events/Resources/Private/Language/locallang_csh_tx_otevents_domain_model_event.xlf');
+//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_otevents_domain_model_event');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_otevents_domain_model_eventcategory', 'EXT:ot_events/Resources/Private/Language/locallang_csh_tx_otevents_domain_model_eventcategory.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_otevents_domain_model_eventcategory');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_otevents_domain_model_eventlocation',
+    'EXT:ot_events/Resources/Private/Language/locallang_csh_tx_otevents_domain_model_eventlocation.xlf');
+//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_otevents_domain_model_eventlocation');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_otevents_domain_model_eventcategory',
+    'EXT:ot_events/Resources/Private/Language/locallang_csh_tx_otevents_domain_model_eventcategory.xlf');
+//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_otevents_domain_model_eventcategory');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable(
     $_EXTKEY,
